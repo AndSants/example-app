e4188688ba6aa96f7e88358a3fafaef7110adb3e
André Santos <andresantos.iron@gmail.com>
2022-02-10 22:27:25 -0300

    add softdeltes in provider db

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class FornecedorController extends Controller
 {
+    
+
+    /**
+     * return view
+     */
     public function index()
     {
         return view('app.fornecedor.index');
+
+        //find(); retorna um ou uma coleção de registros
+        //all(); retorna todos os registros
     }
 }
--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
-    use HasFactory;
+    use HasFactory;     //trait
+    use SoftDeletes;    //trait
+
+    //The database connection that should be used by the model.
+    protected $connection = 'sqlite';
+    //The table associated with the model.
+    protected $table = 'providers';
+    //The primary key associated with the table.
+    protected $primaryKey = 'id';
+    /*
+      Autoriza que os parametros passados por array
+      são adicionados como atributo do próprio objeto
+      para que seja persistido seu valor no banco
+    */
+    protected $fillable = ['name'];
 }
